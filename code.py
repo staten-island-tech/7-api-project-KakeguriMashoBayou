a013bde82a302812b73c6463057c7f0ae4347d6d
KakeguriMashoBayou <boyul@nycstudents.net>
2026-05-07 19:02:44 -0400

    Update code.py

--- code.py
+++ code.py
@@ -2,3 +2,11 @@ import tkinter as tk
 import requests
 from PIL import Image, ImageTk
 
+
+def pet_axolotl():
+    global photo
+    url = 'https://theaxolotlapi.netlify.app/'
+    response = requests.get(url)
+    with open("axolotl.png", "wb") as f:
+        f.write(response.content)
+
