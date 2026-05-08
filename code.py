ba39401d9c8468dfa9f48fdd527284ce7477d252
KakeguriMashoBayou <boyul@nycstudents.net>
2026-05-07 20:08:56 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import tkinter as tk
 import requests
 from PIL import Image, ImageTk
@@ -9,4 +10,7 @@ def pet_axolotl():
     response = requests.get(url)
     with open("axolotl.png", "wb") as f:
         f.write(response.content)
-
+    image = Image.open("axolotl.jpg")
+    photo = ImageTk.PhotoImage(image)
+    label.config(image=photo)
+    label.image = photo
