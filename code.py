7a9d97ec54c4173727457e3471e11327aa27f534
KakeguriMashoBayou <boyul@nycstudents.net>
2026-05-07 21:31:55 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-from cProfile import label
+
 import tkinter as tk
 import requests
 from PIL import Image, ImageTk
@@ -6,9 +6,9 @@ from PIL import Image, ImageTk
 
 def pet_axolotl():
     global photo
-    url = 'https://theaxolotlapi.netlify.app/'
+    url = "https://theaxolotlapi.netlify.app/"
     response = requests.get(url)
-    with open("axolotl.png", "wb") as f:
+    with open("axolotl.jpg", "wb") as f:
         f.write(response.content)
     image = Image.open("axolotl.jpg")
     photo = ImageTk.PhotoImage(image)
@@ -21,3 +21,9 @@ window.geometry("1200x800")
 window.resizeable(False, False)
 prompt = tk.Label(window,text = "Click the button for an axolotl",font = ("Arial, 16"))
 prompt.pack(pady = 10)
+
+button = tk.Button( window, text = "Axolotl", command = pet_axolotl, font = ("Arial", 16), bg = "pink", fg = "blue")
+button.pack(pady = 20)
+label = tk.Label(window)
+label.pack(pady=30)
+window.mainloop()
